31517380bc958f15736f2a047c7dec8592aa254f
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-07 11:28:00 -0700

    HappyBirthday.java complete

--- src/HappyBirthday.java
+++ src/HappyBirthday.java
@@ -6,6 +6,11 @@ public class HappyBirthday {
         String name = scan.nextLine();
         System.out.println("When was they born?");
         int age = scan.nextInt();
-        System.out.println("");
+        System.out.println("What year is it now?");
+        int year = scan.nextInt();
+        for (;age<=year;age++){
+            System.out.println(age);
+            System.out.println("Happy Birthday to you, Happy Birthday to you! Happy Birthday dear Dad, Happy Birthday to you! ");
+        }
     }
 }
