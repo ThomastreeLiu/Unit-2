44d0621d587e32da0f21675d9d2858c10403eca1
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-07 10:55:21 -0700

    FizzBuzz.java complete

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,6 +4,16 @@ public class FizzBuzz {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number");
         int lastNum = scan.nextInt();
+        for (int i = 0;i<=lastNum;i++){
+            if (i%3==0&&i%5==0)
+                System.out.println("Fizz-Buzz");
+            else if (i%3==0)
+                System.out.println("Fizz");
+            else if (i%5==0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+            }
 
+        }
     }
-}
