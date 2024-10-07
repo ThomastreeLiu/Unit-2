b0ecb5ace7b50ba870606bd325ccd8975657fbd8
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-07 11:21:27 -0700

    Create HappyBirthday.java

---
+++ src/HappyBirthday.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HappyBirthday {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is their name?");
+        String name = scan.nextLine();
+        System.out.println("When was they born?");
+        int age = scan.nextInt();
+        System.out.println("");
+    }
+}
