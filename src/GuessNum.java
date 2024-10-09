d44c1ab8a0197f4035170fe3e8b368988cc66a38
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-09 10:50:08 -0700

    HappyBirthday.java complete

---
+++ src/GuessNum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class GuessNum{
+    public static void main (String args[]){
+
+        Scanner scan = new Scanner(System.in);
+        String D = "y";
+        while (D == "y"||D == "Y") {
+            int num1 = (int) (Math.random() * 10) + 1;
+            System.out.println("Guess the number (1-10): ");
+            int ans1 = scan.nextInt();
+
+            while (num1 != ans1) {
+                System.out.println("Nope, try again");
+                System.out.println("Guess the number (1-10): ");
+                ans1 = scan.nextInt();
+            }
+            System.out.println("Right! Good job.");
+            System.out.println("play again? (y or n)");
+            D = scan.next();
+        }
+    }
+
+}
+
+
