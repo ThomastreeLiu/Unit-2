d67542f940ea92d46900820f29a2cf855012bc8c
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-24 10:31:47 -0700

    physics.java complete

--- src/physics.java
+++ src/physics.java
@@ -1,3 +1,8 @@
+//This is not a part of unit 2
+//Author:Thomas
+//Name:Physics
+//Purpose:Enter the data of physics experiment and get answer of it.
+
 import java.util.Scanner;
 public class physics {
     public static void main (String[] args){
