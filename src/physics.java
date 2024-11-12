84ed2e8ead1314ac69dfd9cb5fa5032bf38372ca
ThomastreeLiu <thomastree@126.com>
2024-11-11 20:29:10 -0800

    test operational

--- src/physics.java
+++ src/physics.java
@@ -7,27 +7,20 @@ import java.util.Scanner;
 public class physics {
     public static void main (String[] args){
         Scanner scan = new Scanner(System.in);
-        float vf = 0;
-        float d = 0;
-        float a = 0;
-        float t = 0;
-        float opp = 0;
-        float hyp = 0;
-        String con = "y";
-        while (con.equals("y")){
+        double a = 0;
+        double t = 0;
+        double opp = 0;
+        double hyp = 0;
+        while (1 == 1){
             System.out.println("enter height");
             opp=scan.nextFloat();
             System.out.println("enter the length");
             hyp=scan.nextFloat();
             System.out.println("enter the time");
             t=scan.nextFloat();
-            a= (float) (hyp*2/Math.pow(t,2));
-            vf=a*t;
-            double angle = Math.asin(opp/hyp);
-            double guessa = 9.8*angle;
-            System.out.println("a= "+a+"\nvf= "+vf+"\nthe a from equation is "+guessa);
-            System.out.println("again?(y/n)");
-            con=scan.next();
+            a= 2*hyp/Math.pow(t,2);
+            double guessa = 9.8*opp/hyp;
+            System.out.println("a= "+a+"\nthe a from equation is "+guessa);
         }
     }
 }
