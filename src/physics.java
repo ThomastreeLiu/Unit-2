0840041005f702670b79c4a97f84c0bc2e38d5f6
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-11-12 09:40:40 -0800

    。

--- src/physics.java
+++ src/physics.java
@@ -7,20 +7,42 @@ import java.util.Scanner;
 public class physics {
     public static void main (String[] args){
         Scanner scan = new Scanner(System.in);
+<<<<<<< Updated upstream
+=======
+        double vf = 0;
+        //double d = 0;
+>>>>>>> Stashed changes
         double a = 0;
         double t = 0;
         double opp = 0;
         double hyp = 0;
+<<<<<<< Updated upstream
         while (1 == 1){
+=======
+        String con = "y";
+        while (con.equals("y")){
+>>>>>>> Stashed changes
             System.out.println("enter height");
-            opp=scan.nextFloat();
+            opp=scan.nextDouble();
             System.out.println("enter the length");
-            hyp=scan.nextFloat();
+            hyp=scan.nextDouble();
             System.out.println("enter the time");
+<<<<<<< Updated upstream
             t=scan.nextFloat();
             a= 2*hyp/Math.pow(t,2);
             double guessa = 9.8*opp/hyp;
             System.out.println("a= "+a+"\nthe a from equation is "+guessa);
+=======
+            t=scan.nextDouble();
+            //a= hyp*2/Math.pow(t,2);
+            a = 2*hyp/Math.pow(t,2);
+            vf=a*t;
+            //double angle = Math.asin(opp/hyp);
+            //double guessa = 9.8*Math.sin(angle);
+            System.out.println("a= "+a+"\nvf= "+vf);
+            System.out.println("again?(y/n)");
+            con=scan.next();
+>>>>>>> Stashed changes
         }
     }
 }
